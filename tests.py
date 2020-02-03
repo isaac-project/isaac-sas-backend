dd632de59265c96dffa1daa8ba89be5f9d714de2
Ramon Ziai <ramon.ziai@uni-tuebingen.de>
2020-02-03 09:47:11 +0100

    first test of reading actual CAS, based on hacky cassis fix

--- tests.py
+++ tests.py
@@ -1,10 +1,18 @@
 import unittest
+from features import * 
+from cassis import *
 
+class ReadCASTestCase(unittest.TestCase):
+    def test_read(self):
+        dkpro_ts = load_dkpro_core_typesystem()
+        isaac_ts_file = "features/isaac-type-system.xml"
+        with open(isaac_ts_file, 'rb') as f:
+            typesystem = load_typesystem(f, dkpro_ts)
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
+        file_to_read = "data/xmi/1ET5_1_25.xmi"
+        with open(file_to_read, 'rb') as f:
+            cas = load_cas_from_xmi(f, typesystem=typesystem)
+        self.assertIsNotNone(cas)
 
 if __name__ == '__main__':
     unittest.main()
