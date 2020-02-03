1f394f51fe9f9aeed2c11893a2a4b610653bb68c
Ramon Ziai <ramon@ziai.eu>
2020-02-03 17:09:49 +0100

    implemented Outcome, Diagnosis, KeywordOverlap and LC_TokenMatch

--- features/extractor.py
+++ features/extractor.py
@@ -1,11 +1,92 @@
 from abc import ABC,abstractmethod
 from cassis import Cas
+from enum import Enum, auto
+
+class AlignmentLabel(Enum):
+    LC_TOKEN = auto()
+    TOKEN = auto()
+    DEPTRIPLE = auto()
+    SEMTYPE = auto()
+    LEMMA = auto()
+    SPELLING = auto()
+    SYNONYM = auto()
+    SIMILARITY = auto()
+    REFERENCE = auto()
 
 class FeatureExtractor(ABC):
+    # some constants common to feature extractors
     STUDENT_ANSWER_VIEW = "studentAnswer"
-    TARGET_ANSWER_VIEW = "_initialView"
+    TARGET_ANSWER_VIEW = "_InitialView"
     QUESTION_VIEW = "question"
-
+    ANSWER_TYPE = "de.sfs.isaac.server.nlp.types.Answer"
+    MAPPABLE_TYPE = "de.sfs.isaac.server.nlp.types.MappableAnnotation"
+    TOKEN_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"
+    
     @abstractmethod
     def extract(self, cas: Cas) -> float:
         pass
+    
+    def getPercOfMappingType(self, cas: Cas, alignment: AlignmentLabel) -> float:
+        overallMatchesCount = 0
+        itemsOfGivenTypeCount = 0
+        for t in cas.select(self.TOKEN_TYPE):
+
+            item = next(cas.select_covered(self.MAPPABLE_TYPE, t));
+
+            # check for matches/alignment
+            if item.match is not None and item.match.target is not None:
+                overallMatchesCount += 1
+                # check for types
+                if item.match.label == alignment.name:
+                    itemsOfGivenTypeCount += 1
+
+        # if nothing has been matched at all, the result is 0
+        if overallMatchesCount == 0:
+            return 0.0;
+        else:
+            return itemsOfGivenTypeCount / overallMatchesCount
+
+class Outcome(FeatureExtractor):
+    LABEL2INT = {"correct" : 1, "incorrect" : 0, "true" : 1, "false" : 0}
+    
+    def extract(self, cas: Cas) -> float:
+        studentView = cas.get_view(self.STUDENT_ANSWER_VIEW)
+        answer = next(studentView.select(self.ANSWER_TYPE))
+        score = answer.contentScore
+        
+        if score in self.LABEL2INT:
+            return self.LABEL2INT[score]
+        else:
+            return int(answer.contentScore)
+
+class Diagnosis(FeatureExtractor):
+    
+    def extract(self, cas:Cas)->float:
+        studentView = cas.get_view(self.STUDENT_ANSWER_VIEW)
+        answer = next(studentView.select(self.ANSWER_TYPE))
+        return int(answer.contentDiagnosis)
+
+class KeywordOverlap(FeatureExtractor):
+    KEYWORD_TYPE = "de.sfs.isaac.server.nlp.types.Keyword"
+    
+    def extract(self, cas:Cas)->float:
+        targetView = cas.get_view(self.TARGET_ANSWER_VIEW)
+        cas.select
+        matchedKeywords = 0
+        allKeywords = 0
+        for k in targetView.select(self.KEYWORD_TYPE):
+            allKeywords += 1
+            mappable = next(targetView.select_covered(self.MAPPABLE_TYPE, k))
+            if mappable.match:
+                matchedKeywords += 1
+        
+        if not allKeywords:
+            return 0.0
+        else:
+            return matchedKeywords / allKeywords
+    
+class LC_TokenMatch(FeatureExtractor):
+    
+    def extract(self, cas:Cas)->float:
+        studentView = cas.get_view(self.STUDENT_ANSWER_VIEW)
+        return self.getPercOfMappingType(studentView, AlignmentLabel.LC_TOKEN)
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
-from features import * 
 from cassis import *
+from features.extractor import Outcome, KeywordOverlap, LC_TokenMatch
 
 class ReadCASTestCase(unittest.TestCase):
     def test_load_typesystem(self):
@@ -10,11 +10,6 @@ class ReadCASTestCase(unittest.TestCase):
             typesystem = load_typesystem(f)
 
         final_ts = merge_typesystems(dkpro_ts,typesystem)
-        
-        for t in final_ts.get_types():
-            print(t.name)
-            for feat in t.features:
-                print(feat.name)
         
         self.assertIsInstance(final_ts, TypeSystem)
 
@@ -30,5 +25,47 @@ class ReadCASTestCase(unittest.TestCase):
             cas = load_cas_from_xmi(f, typesystem=final_ts)
         self.assertIsInstance(cas, Cas)
         
+    def test_extract_outcome(self):
+        dkpro_ts = load_dkpro_core_typesystem()
+        isaac_ts_file = "features/isaac-type-system.xml"
+        with open(isaac_ts_file, 'rb') as f:
+            isaac_ts = load_typesystem(f)
+
+        final_ts = merge_typesystems(dkpro_ts,isaac_ts)
+        file_to_read = "data/xmi/1ET5_7_0.xmi"
+        with open(file_to_read, 'rb') as f:
+            cas = load_cas_from_xmi(f, typesystem=final_ts)
+        
+        o = Outcome().extract(cas)
+        self.assertTrue(o)
+        
+    def test_extract_kw_overlap(self):
+        dkpro_ts = load_dkpro_core_typesystem()
+        isaac_ts_file = "features/isaac-type-system.xml"
+        with open(isaac_ts_file, 'rb') as f:
+            isaac_ts = load_typesystem(f)
+
+        final_ts = merge_typesystems(dkpro_ts,isaac_ts)
+        file_to_read = "data/xmi/1ET5_7_0.xmi"
+        with open(file_to_read, 'rb') as f:
+            cas = load_cas_from_xmi(f, typesystem=final_ts)
+        
+        kw = KeywordOverlap().extract(cas)
+        self.assertTrue(kw > 0.0);
+    
+    def test_extract_lc_token_overlap(self):
+        dkpro_ts = load_dkpro_core_typesystem()
+        isaac_ts_file = "features/isaac-type-system.xml"
+        with open(isaac_ts_file, 'rb') as f:
+            isaac_ts = load_typesystem(f)
+
+        final_ts = merge_typesystems(dkpro_ts,isaac_ts)
+        file_to_read = "data/xmi/1ET5_7_0.xmi"
+        with open(file_to_read, 'rb') as f:
+            cas = load_cas_from_xmi(f, typesystem=final_ts)
+        
+        lc = LC_TokenMatch().extract(cas)
+        self.assertTrue(lc > 0.0);
+                
 if __name__ == '__main__':
     unittest.main()
