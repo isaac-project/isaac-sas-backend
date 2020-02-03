c6701dc518cd4d53a86a4cf5ef78427c7dc50c07
Ramon Ziai <ramon@ziai.eu>
2020-02-03 13:27:11 +0100

    Working tests for loading our typesystem and CAS

--- tests.py
+++ tests.py
@@ -3,16 +3,32 @@ from features import *
 from cassis import *
 
 class ReadCASTestCase(unittest.TestCase):
-    def test_read(self):
+    def test_load_typesystem(self):
         dkpro_ts = load_dkpro_core_typesystem()
         isaac_ts_file = "features/isaac-type-system.xml"
         with open(isaac_ts_file, 'rb') as f:
-            typesystem = load_typesystem(f, dkpro_ts)
+            typesystem = load_typesystem(f)
 
-        file_to_read = "data/xmi/1ET5_1_25.xmi"
+        final_ts = merge_typesystems(dkpro_ts,typesystem)
+        
+        for t in final_ts.get_types():
+            print(t.name)
+            for feat in t.features:
+                print(feat.name)
+        
+        self.assertIsInstance(final_ts, TypeSystem)
+
+    def test_read_cas(self):
+        dkpro_ts = load_dkpro_core_typesystem()
+        isaac_ts_file = "features/isaac-type-system.xml"
+        with open(isaac_ts_file, 'rb') as f:
+            isaac_ts = load_typesystem(f)
+
+        final_ts = merge_typesystems(dkpro_ts,isaac_ts)
+        file_to_read = "data/xmi/1ET5_7_0.xmi"
         with open(file_to_read, 'rb') as f:
-            cas = load_cas_from_xmi(f, typesystem=typesystem)
-        self.assertIsNotNone(cas)
-
+            cas = load_cas_from_xmi(f, typesystem=final_ts)
+        self.assertIsInstance(cas, Cas)
+        
 if __name__ == '__main__':
     unittest.main()
