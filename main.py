fb8d2df8e17dcee9baec828b02011cccc8796c79
Ramon Ziai <ramon.ziai@uni-tuebingen.de>
2021-06-30 13:33:56 +0200

    More docs

--- main.py
+++ main.py
@@ -74,32 +74,24 @@ for bow_file in os.listdir(bow_model_dir):
 
 
 class LanguageDataRequest(BaseModel):
-    """
-    A request with language data, used for training and predicting.
-    """
+    """A request with language data, used for training and predicting."""
     instances: List[ShortAnswerInstance]
     modelId: str
 
 
 class SinglePrediction(BaseModel):
-    """
-    A single prediction result, including probabilities for individual classes.
-    """
+    """A single prediction result, including probabilities for individual classes."""
     prediction: Union[int, str]
     classProbabilities: Dict[Union[str, int], float]
 
 
 class PredictFromLanguageDataResponse(BaseModel):
-    """
-    A response containing one or more prediction results.
-    """
+    """A response containing one or more prediction results."""
     predictions: List[SinglePrediction]
 
 
 class ModelIdResponse(BaseModel):
-    """
-    A response containing the IDs of the models currently available.
-    """
+    """A response containing the IDs of the models currently available."""
     modelIds: List[str]
 
 
